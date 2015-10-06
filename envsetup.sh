fc98dd6ab223ffe07f2772354f391f91033401b5
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2015-10-06 18:01:07 -0400

    envsetup: add the mka and reposync functions
    Change-Id: I05457205c68395c0e92b2691df397fbc067e786e

--- envsetup.sh
+++ envsetup.sh
@@ -26,6 +26,8 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sepgrep:   Greps on all local sepolicy files.
 - sgrep:     Greps on all local source files.
 - godir:     Go to the directory containing a file.
+- mka:       Builds using SCHED_BATCH on all processors
+- reposync:  Parallel repo sync using ionice and SCHED_BATCH
 
 Environment options:
 - SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
@@ -1597,7 +1599,6 @@ function godir () {
     \cd $T/$pathname
 }
 
-# Make using all available CPUs
 function mka() {
     case `uname -s` in
         Darwin)
@@ -1605,6 +1606,17 @@ function mka() {
             ;;
         *)
             schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
+            ;;
+    esac
+}
+
+function reposync() {
+    case `uname -s` in
+        Darwin)
+            repo sync -j 4 "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 `which repo` sync -j 4 "$@"
             ;;
     esac
 }
