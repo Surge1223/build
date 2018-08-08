5933e67bddb59a39f1bf35e1db929eb47cf19196
Surge Raval <Surge1223@gmail.com>
2018-08-08 22:05:35 +0000

    build: add custom build info back
    Change-Id: I65f4c8358731f0365ec14d4cb16c10cec5e45a79

--- envsetup.sh
+++ envsetup.sh
@@ -131,11 +131,11 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-
     if (echo -n $1 | grep -q -e "^aosp_") ; then
-       CUSTOM_BUILD=
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^aosp_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $CUSTOM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
-       CUSTOM_BUILD=$1
+        CUSTOM_BUILD=
     fi
     export CUSTOM_BUILD
 
